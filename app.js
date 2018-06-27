e94a47b4c18c5ccb0a2ad1ba7473ebfdad0c6bf7
unknown <andiemontoyeah@gmail.com>
2018-06-26 20:28:49 -0700

    change port to be dynamically set instead of fixed number

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/user.route');
 
 app.use('/user', userRoutes);  
 
-let port = 1234;
+let port = process.env.PORT || 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
